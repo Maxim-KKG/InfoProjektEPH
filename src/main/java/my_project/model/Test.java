5dff740751aab80bfbe858bb0d58b0166e49f394
Maxim-KKG <goldmaxi@kkg-do.eu>
2023-05-23 20:24:00 +0200

    Changes by alex2

--- src/main/java/my_project/model/Test.java
+++ src/main/java/my_project/model/Test.java
@@ -1,5 +1,14 @@
 package my_project.model;
 
 public class Test {
-
+    /**
+     * Gameplay-ideas:
+     * basically Ducktato
+     * --Arena: Etwas Größer als der Bildschirm
+     * --Player: kein Fahrzeug, vielleicht Ente, Wächst mit Brot(Kücken -> Ente)
+     * --Waffen: Alles, Basic attack ist Eier werfen
+     * --Setting: Sumpf
+     * --Gegner: Natürliche Feine, Droppen Brot, Spawnen in Wellen
+     * --Item System: alles zählt, Shop
+     */
 }
