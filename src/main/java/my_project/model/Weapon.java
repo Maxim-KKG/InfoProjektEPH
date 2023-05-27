301fdc0a8f8890bf6b8e68d0a5fcd4c39a20b375
Maxim-KKG <goldmaxi@kkg-do.eu>
2023-05-27 15:53:44 +0200

    AAAAAAAAAAAAA

---
+++ src/main/java/my_project/model/Weapon.java
@@ -0,0 +1,9 @@
+package my_project.model;
+
+import KAGO_framework.model.GraphicalObject;
+
+public abstract class Weapon extends GraphicalObject {
+    public Weapon(){
+
+    }
+}
---
+++ src/main/java/my_project/model/weapons/Egg.java
@@ -0,0 +1,9 @@
+package my_project.model.weapons;
+
+import my_project.model.Weapon;
+
+public class Egg extends Weapon {
+    public Egg(){
+
+    }
+}
